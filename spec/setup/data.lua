f59c18ac0e397b7123f3ea533f7f8e5ae7737fc0
goakiller900 <remco.kappers@gmail.com>
2026-07-12 18:54:17 +0200

    Reset translated Faketorio fixture modules

--- spec/setup/data.lua
+++ spec/setup/data.lua
@@ -2,7 +2,8 @@ local DataFixture = {}
 
 function DataFixture.reload()
     for module_name in pairs(package.loaded) do
-        if module_name == 'faketorio/dataloader' or module_name:match('^faketorio/raw/') then
+        if module_name:find('faketorio/dataloader', 1, true)
+            or module_name:find('faketorio/raw/', 1, true) then
             package.loaded[module_name] = nil
         end
     end
